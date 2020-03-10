a6c4a766902a2a8e59eb0ca5c1f661cce24a957b
Tai N <ndhuutai1@gmail.com>
2020-03-09 17:03:28 -0700

    modified redirect uri

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -22,7 +22,7 @@ const onRedirectCallback = appState => {
 ReactDOM.render( <Auth0Provider
 	domain={process.env.REACT_APP_DOMAIN}
 	client_id={process.env.REACT_APP_CLIENT_ID}
-	redirect_uri={`${window.location.origin}/admin`}
+	redirect_uri={`https://fire-nation.herokuapp.com/admin`}
 	audience={process.env.REACT_APP_AUDIENCE}
 	onRedirectCallback={onRedirectCallback}
 >
